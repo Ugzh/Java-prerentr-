39f37e78f46771f75069b7cba770426048317f31
Ugo Thevenin <ugzher@Ugos.lan>
2024-12-04 14:57:16 +0100

    Gestion d'ajout effectif

--- Contacts.java
+++ Contacts.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Contacts {
     public static void main(String[] args) {
         byte choice;
+        byte select;
         ArrayList<ManageContact> addContact = new ArrayList<ManageContact>();
         Scanner keyboardListener = new Scanner(System.in);
         do {
@@ -10,7 +11,14 @@ public class Contacts {
             choice = keyboardListener.nextByte();
             switch (choice){
                 case 1 :
+                    System.out.println("Ajouter un nouveau contact");
+                    System.out.println("");
                     addContact.add(new ManageContact());
+                    do {
+                        menu.getMenuByCategory("add");
+                        select = keyboardListener.nextByte();
+                        if(select == 1) addContact.add(new ManageContact());
+                    } while (select != 2);
                     break;
                 case 2 :
                     System.out.println("choix 2");
--- Menu.java
+++ Menu.java
@@ -7,4 +7,26 @@ public class Menu {
         System.out.println("4 -> Quitter");
         System.out.println("");
     }
+
+    public void getMenuByCategory(String title){
+        switch (title){
+            case "add":
+                System.out.println("");
+                System.out.println("1 -> Ajouter un contact");
+                System.out.println("2 -> Retour au menu principal");
+                break;
+            case "search":
+                System.out.println("");
+                System.out.println("1 -> Cherche un nouveau contact");
+                System.out.println("2 -> Retour au menu principal");
+                break;
+            case "show":
+                System.out.println("");
+                System.out.println("1 -> Lister les contacts à nouveau");
+                System.out.println("2 -> Retour au menu principal");
+                break;
+            default:
+                System.out.println("Il n'y a pas de menu à afficher");
+        }
+    }
 }
